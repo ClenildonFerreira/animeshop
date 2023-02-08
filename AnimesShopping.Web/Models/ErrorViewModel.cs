8e2c6fdeef742950c2e537796a9dc798b5d9e4eb
ClenildonFerreira <clenildonferreira34@gmail.com>
2023-02-08 13:43:52 -0300

    commit first

---
+++ AnimesShopping.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AnimesShopping.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
